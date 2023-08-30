b14046385cf654cded32c916f91dfb255f4bbc3c
=Nagesh <=nageshg429@gmail.com>
2023-08-30 07:50:16 +0530

    to do app completed tasks ui created

--- TodoListApp/script.js
+++ TodoListApp/script.js
@@ -2,10 +2,12 @@ const input = document.getElementById('inputbox');
 const btn = document.getElementById('btn');
 const list = document.getElementById('list');
 const li = document.getElementsByTagName('li');
+const clist = document.getElementById('clist');
+const date = document.getElementById('date');
 btn.addEventListener('click',function(){
-    if(input.value=='')
+    if(input.value=='' || date.value =='')
      {
-        alert("please add your task");
+        alert("please Verify Your Creation of Task and date");
      }
      else{
         let li = document.createElement('li');
@@ -13,15 +15,32 @@ btn.addEventListener('click',function(){
         list.appendChild(li);
         let span = document.createElement('span');
         span.innerHTML = "\u00d7";
+        let d = document.createElement('p');
+        d.innerHTML = new Date(date.value);
+       let  stdate = new Date(date.value);
+        d.classList.add('date');
+        li.appendChild(d);
         li.appendChild(span);
      }
      input.value='';
+
 })
+
+/* &#10003; */
 list.addEventListener('click' , function(e){
     if(e.target.tagName === 'LI'){
-        e.target.classList.toggle("checked");
+
+        let date = new Date();
+        let c1 = document.createElement('p');
+        let  s = document.createElement('span');
+        s.innerHTML = "&#10003;";
+        c1.innerHTML = e.srcElement.childNodes[0].data ;
+        c1.appendChild(s);
+        clist.appendChild(c1);
+        e.srcElement.remove();
+
     }
     else if(e.target.tagName === 'SPAN'){
         e.target.parentElement.remove();
     }
-})
+},false)
